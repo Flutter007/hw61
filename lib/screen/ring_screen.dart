93cff4e084ff5c10d33d02a30dd940465b479519
Brug <alekseybrug@gmail.com>
2025-06-11 20:52:03 +0600

    Added RotationTransition Animation for Icon.Made StateFulWidget for Ring Screen,added some design for App.

---
+++ lib/screen/ring_screen.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+
+class RingScreen extends StatefulWidget {
+  const RingScreen({super.key});
+
+  @override
+  State<RingScreen> createState() => _RingScreenState();
+}
+
+class _RingScreenState extends State<RingScreen>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+  int count = 0;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _animationController = AnimationController(
+      vsync: this,
+      duration: Duration(seconds: 1),
+      reverseDuration: Duration(seconds: 1),
+    );
+    _animationController.value = 0.65;
+
+    _animationController.addStatusListener((status) {
+      if (status == AnimationStatus.completed && count < 6) {
+        count++;
+        _animationController.reverse();
+      } else if (status == AnimationStatus.dismissed && count < 6) {
+        count++;
+        _animationController.forward();
+      } else if (count == 6) {
+        _animationController.stop();
+        _animationController.value = 0.65;
+      }
+    });
+  }
+
+  void runAnimation() {
+    count = 0;
+    _animationController.forward();
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Ring Notification'),
+        backgroundColor: theme.colorScheme.errorContainer,
+      ),
+      body: Center(
+        child: AnimatedBuilder(
+          animation: _animationController,
+          child: IconButton(
+            onPressed: runAnimation,
+            icon: Icon(
+              Icons.notifications_active,
+              size: 160,
+              color: theme.colorScheme.error,
+            ),
+          ),
+          builder:
+              (ctx, child) => RotationTransition(
+                alignment: Alignment.topCenter,
+                turns: Tween<double>(begin: -0.1, end: 0.1).animate(
+                  CurvedAnimation(
+                    parent: _animationController,
+                    curve: Curves.fastOutSlowIn.flipped,
+                    reverseCurve: Curves.fastOutSlowIn.flipped,
+                  ),
+                ),
+                child: child,
+              ),
+        ),
+      ),
+    );
+  }
+}
